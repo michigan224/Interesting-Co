e7a6f42ccc55068f0e54e9a1fddbc16d0fcd2a70
smhavens <smhavens@umich.edu>
2022-03-14 19:43:54 -0400

    Some button implementations in Friends tab.

--- rememri/app/src/main/java/edu/umich/interestingco/rememri/AccountActivity.kt
+++ rememri/app/src/main/java/edu/umich/interestingco/rememri/AccountActivity.kt
@@ -1,2 +1,13 @@
 package edu.umich.interestingco.rememri
 
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class AccountActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
--- rememri/app/src/main/java/edu/umich/interestingco/rememri/FriendActivity.kt
+++ rememri/app/src/main/java/edu/umich/interestingco/rememri/FriendActivity.kt
@@ -1,7 +1,9 @@
 package edu.umich.interestingco.rememri
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import androidx.viewpager.widget.ViewPager
 import com.google.android.material.tabs.TabLayout
 
@@ -9,6 +11,7 @@ class FriendActivity : AppCompatActivity() {
 
     var tabLayout: TabLayout? = null
     var viewPager: ViewPager? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_friend)
@@ -39,5 +42,9 @@ class FriendActivity : AppCompatActivity() {
         })
 
     }
+
+    fun returnMain(view: View?) = startActivity(Intent(this, MainActivity::class.java))
+
+    fun returnAccount(view: View?) = startActivity(Intent(this, AccountActivity::class.java))
 }
 
