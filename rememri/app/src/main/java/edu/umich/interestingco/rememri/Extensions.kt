626444e1f85fa90d3877c9134a5a082459c2ef7b
russecla <russecla@umich.edu>
2022-03-15 04:33:22 -0400

    added comment

--- rememri/app/src/main/java/edu/umich/interestingco/rememri/Extensions.kt
+++ rememri/app/src/main/java/edu/umich/interestingco/rememri/Extensions.kt
@@ -3,6 +3,7 @@ package edu.umich.interestingco.rememri
 import android.content.Context
 import android.widget.Toast
 
+// Used in Main Activity for the camera functionality
 fun Context.toast(message: String, short: Boolean = true) {
     Toast.makeText(this, message, if (short) Toast.LENGTH_SHORT else Toast.LENGTH_LONG).show()
 }
