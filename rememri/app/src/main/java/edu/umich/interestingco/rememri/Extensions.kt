f44f963d4cbec8b89e01d005bd06b3312d3f7adf
russecla <russecla@umich.edu>
2022-03-15 04:06:58 -0400

    Revert "added for picture functionality"
    This reverts commit bedb4f0546265cb926b00d83bad5d265e95de308.

--- rememri/app/src/main/java/edu/umich/interestingco/rememri/Extensions.kt
+++
@@ -1,8 +0,0 @@
-package edu.umich.interestingco.rememri
-
-import android.content.Context
-import android.widget.Toast
-
-fun Context.toast(message: String, short: Boolean = true) {
-    Toast.makeText(this, message, if (short) Toast.LENGTH_SHORT else Toast.LENGTH_LONG).show()
-}
