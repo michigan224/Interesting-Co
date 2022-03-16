76f8be76afbd006b89d67782176d7f3382ec3f60
colyoon <colyoon@umich.edu>
2022-03-16 19:42:17 -0400

    Added single image view on update

--- rememri/app/src/main/java/edu/umich/interestingco/rememri/ARView.java
+++ rememri/app/src/main/java/edu/umich/interestingco/rememri/ARView.java
@@ -14,10 +14,23 @@ import android.widget.Toast;
 import com.google.ar.core.Anchor;
 import com.google.ar.core.HitResult;
 import com.google.ar.core.Plane;
+import com.google.ar.core.Pose;
+import com.google.ar.core.Session;
 import com.google.ar.sceneform.AnchorNode;
 import com.google.ar.sceneform.rendering.ModelRenderable;
+import com.google.ar.sceneform.rendering.ViewRenderable;
 import com.google.ar.sceneform.ux.ArFragment;
 import com.google.ar.sceneform.ux.TransformableNode;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import android.os.Handler;
+import android.widget.ImageView;
+import android.graphics.drawable.Drawable;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import edu.umich.interestingco.rememri.R;
+import android.view.ViewGroup;
 
 public class ARView extends AppCompatActivity {
     private static final String TAG = ARView.class.getSimpleName();
@@ -25,6 +38,10 @@ public class ARView extends AppCompatActivity {
 
     private ArFragment arFragment;
     private ModelRenderable andyRenderable;
+    private ImageView image_view;
+
+    private ViewRenderable imageRenderable;
+    private boolean hasFinishedLoading = false;
 
     @Override
     @SuppressWarnings({"AndroidApiChecker", "FutureReturnValueIgnored"})
@@ -40,8 +57,77 @@ public class ARView extends AppCompatActivity {
         setContentView(R.layout.activity_ar);
         arFragment = (ArFragment) getSupportFragmentManager().findFragmentById(R.id.ux_fragment);
 
+        CompletableFuture<ViewRenderable> imageStage =
+                ViewRenderable.builder().setView(this, R.layout.image_view).build();
 
+        CompletableFuture.allOf(
+                imageStage)
+                .handle(
+                        (notUsed, throwable) -> {
+
+                            if (throwable != null) {
+                                return null;
+                            }
+
+                            try {
+                                imageRenderable = imageStage.get();
+
+                                // Everything finished loading successfully.
+                                hasFinishedLoading = true;
+
+                            } catch (InterruptedException | ExecutionException ex) {
+                            }
+
+                            return null;
+                        });
+
+        final Handler handler = new Handler();
+        final int delay = 10000; // 1000 milliseconds == 1 second
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                DisplayImage();
+                handler.postDelayed(this, delay);
+            }
+        }, 5000);
     }
+
+    public void DisplayImage() {
+        ImageView i = new ImageView(this);
+        i.setImageResource(R.drawable.goat);
+        i.setAdjustViewBounds(true);
+        i.setLayoutParams(new ViewGroup.LayoutParams(
+                ViewGroup.LayoutParams.WRAP_CONTENT,
+                ViewGroup.LayoutParams.WRAP_CONTENT));
+
+        CompletableFuture<ViewRenderable> imageStage =
+                ViewRenderable.builder().setView(this, i).build();
+        CompletableFuture.allOf(
+                imageStage)
+                .handle(
+                        (notUsed, throwable) -> {
+
+                            if (throwable != null) {
+                                return null;
+                            }
+
+                            try {
+                                Session session = arFragment.getArSceneView().getSession();
+                                float[] pos = { 0, 0, -1 };
+                                float[] rotation = { 0, 0, 0, 1 };
+                                Anchor anchor =  session.createAnchor(new Pose(pos, rotation));
+                                AnchorNode anchorNode = new AnchorNode(anchor);
+                                anchorNode.setRenderable(imageStage.get());
+                                anchorNode.setParent(arFragment.getArSceneView().getScene());
+
+                            } catch (InterruptedException | ExecutionException ex) {
+                            }
+
+                            return null;
+                        });
+    }
+
 
     /**
      * Returns false and displays an error message if Sceneform can not run, true if Sceneform can run
