de6d5ff31fb3c0f2aa220fb16d863677fdb06d4c
russecla <russecla@umich.edu>
2022-03-17 16:44:42 -0400

    Revert "Merge branch 'frontend'"
    This reverts commit 03b345d335370e9d31fa0f75e74124aac2d7b1db, reversing
    changes made to 19279d7b2948b315d07abb7ecf4e6dca981160c6.

--- rememri/app/src/main/java/edu/umich/interestingco/rememri/Friend.kt
+++
@@ -1,5 +0,0 @@
-package edu.umich.interestingco.rememri
-
-class Friend(var username: String? = null,
-             var relationship: String? = null)  {
-}
--- rememri/app/src/main/java/edu/umich/interestingco/rememri/PostActivity.kt
+++
@@ -1,2 +0,0 @@
-package edu.umich.interestingco.rememri
-
