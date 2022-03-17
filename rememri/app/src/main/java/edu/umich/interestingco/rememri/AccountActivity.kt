204740d2f544b1e82f3b0b05258966791d0f8b38
russecla <russecla@umich.edu>
2022-03-17 12:17:57 -0400

    added basic account page

--- rememri/app/src/main/java/edu/umich/interestingco/rememri/AccountActivity.kt
+++ rememri/app/src/main/java/edu/umich/interestingco/rememri/AccountActivity.kt
@@ -8,6 +8,12 @@ import android.view.View
 class AccountActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        print("made is this far")
         setContentView(R.layout.activity_account)
     }
+
+    fun returnMain(view: View?) = startActivity(Intent(this, MainActivity::class.java))
+
+    fun returnFriends(view: View?) = startActivity(Intent(this, FriendActivity::class.java))
 }
+
--- rememri/app/src/main/java/edu/umich/interestingco/rememri/MainActivity.kt
+++ rememri/app/src/main/java/edu/umich/interestingco/rememri/MainActivity.kt
@@ -132,6 +132,8 @@ class MainActivity : AppCompatActivity() {
 
     fun returnFriends(view: View?) = startActivity(Intent(this, FriendActivity::class.java))
 
+    fun returnAccount(view: View?) = startActivity(Intent(this, AccountActivity::class.java))
+
     private fun initCropIntent(): Intent? {
         // Is there any published Activity on device to do image cropping?
         val intent = Intent("com.android.camera.action.CROP")
