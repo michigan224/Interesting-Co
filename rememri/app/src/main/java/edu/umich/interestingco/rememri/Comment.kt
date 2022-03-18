a663f2c3c1273e09c2f5b5dc6789819996006d31
David Sanabria <dc_sanabria@yahoo.com>
2022-03-17 23:25:13 -0400

    Revert "Frontend + AR"

--- rememri/app/src/main/java/edu/umich/interestingco/rememri/Comment.kt
+++
@@ -1,9 +0,0 @@
-package edu.umich.interestingco.rememri
-
-class Comment(var comment_id: Int? = null,
-              var text: String? = null,
-              var timestamp: String? = null,
-              var post_id: Int? = null,
-              var owner_id: String? = null,
-              var is_owned_by_user: Boolean? = null) {
-}
