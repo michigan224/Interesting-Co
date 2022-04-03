1bac810e9dcb838439d664b54384e3d22d762574
smhavens <smhavens@umich.edu>
2022-04-02 23:27:35 -0400

    Added the basis for the rest of the friend view (except the adapter for friend request list)

--- rememri/app/src/main/java/edu/umich/interestingco/rememri/FriendAddFragment.kt
+++ rememri/app/src/main/java/edu/umich/interestingco/rememri/FriendAddFragment.kt
@@ -1,10 +1,15 @@
 package edu.umich.interestingco.rememri
 
+import android.content.Context
+import android.content.SharedPreferences
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ListView
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -24,6 +29,20 @@ class FriendAddFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
+
+        val sharedPref : SharedPreferences?= activity?.getPreferences(Context.MODE_PRIVATE)
+        val username = sharedPref?.getString("username", "")
+
+        val view: View = inflater.inflate(R.layout.fragment_friend_add, container, false)
+        val submitBtn = view.findViewById<Button>(R.id.submitButton)
+        val usernameSearch = view.findViewById<EditText>(R.id.editTextSearch)
+
+        submitBtn.setOnClickListener {
+            val submitSearch = usernameSearch.text
+            var apiRequest : String =
+                "https://rememri-instance-5obwaiol5q-ue.a.run.app/friend_request?user1=$username&user2=$submitSearch&action=request"
+        }
+
         return inflater.inflate(R.layout.fragment_friend_add, container, false)
     }
 }
