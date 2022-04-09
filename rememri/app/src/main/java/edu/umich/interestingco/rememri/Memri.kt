743401e412e4aeac56041142d120375e1594d375
russecla <russecla@umich.edu>
2022-04-08 23:39:04 -0400

    added class variable to hold is_friend parameter

--- rememri/app/src/main/java/edu/umich/interestingco/rememri/Memri.kt
+++ rememri/app/src/main/java/edu/umich/interestingco/rememri/Memri.kt
@@ -3,6 +3,7 @@ package edu.umich.interestingco.rememri
 class Memri(var post_id: Int? = null,
             var owner_id: String? = null,
             var is_public: Boolean? = null,
+            var is_friend: Boolean? = null,
             var media_url: String? = null,
             var timestamp: String? = null,
             var comments: Array<Comment>? = null,
